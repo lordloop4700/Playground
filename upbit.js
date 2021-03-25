b46a3a261d960490adf4911785401894d71f3ba4
lordloop4700 <lordloop4700@gmail.com>
2021-03-26 00:04:25 +0900

    2021-03-26 upbit api url

---
+++ upbit.js
@@ -0,0 +1,10 @@
+https://crix-api-tv.upbit.com/v1/crix/tradingview/history?symbol=BTCKRW&resolution=1D&from=1616508511&to=1616681311
+
+https://crix-api-cdn.upbit.com/v1/crix/candles/minutes/30?code=CRIX.UPBIT.KRW-BTC&count=2&to=2021-03-25T14%3A09%3A01Z&timestamp=1616681342596
+
+https://crix-api-cdn.upbit.com/v1/crix/candles/lines?code=CRIX.UPBIT.KRW-BTC
+
+
+
+https://api.upbit.com/v1/market/all
+https://api.upbit.com/v1/ticker?markets=KRW-BTC
