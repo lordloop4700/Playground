6579d276afa54ba5c936e18ec5f4075620c31158
조성윤 <lordloop4700@gmail.com>
2021-04-07 13:41:43 +0900

    2021-04-07 remark remove

--- finddbmachine.js
+++ finddbmachine.js
@@ -1,18 +1,3 @@
-//이 nodejs는 특정 사용자를 입력받아서 그 사용자의 모든 머신에 대한 values값을 준다.(비율값)
-
-/*
-    연산 방법
-    - 파일코인을 비트코인 가격으로 계산 -
-    파일코인 1개가격(원) / 비트코인 1개가격(원)
-
-    - 사용자 개인이 가지고 있는 파일코인 개수 -
-    1번 머신이 가진 코인 / user1의 지분
-    84번 머신이 가진 코인 / user1의 지분
-    
-    위 처럼 생기는 모든 식의 총합
-
-*/
-
 const model = require('../models');
 
 const DBconnection = async () => { //filecoin db랑 연결
@@ -38,9 +23,6 @@ const UserHaveMachine = async (userid_p) => {
             where: {userId: userid_p},
             attibutes: [ "value" , "machineId"] //해당 유저가 각 머신에 얼마의 비율을 가지고 있는지
         });
-
-        //******************값을 가져와서 front에 보여줘야하는 모든 함수는 이곳에 정의 할까? 예를 들면 admin이 특정 유저에 대해서, 그 유저가 각 머신에 대해서 얼마의 지분을 가지고 있는지 조차도... */
-
         /*
         let count = data.length;
         let machine_data = {};
@@ -69,21 +51,3 @@ module.exports = {
     FinduserId,
     UserHaveMachine
 }
-
-
-
-//let usercount = check();
-//console.log(usercount);
-/*
-    머신 번호에 따라서 자동으로 가져올 수 있도록 제작해야함.
-    이 nodejs는 db에서 a라는 유저가 f1머신에서의 지분을 받아서 ejs로 쏘도록 만들어야함.
-    특히 a라는 유저가 f1, f2라는 복수의 서버에 대해서 지분을 가지고 있을 경우 이를 db에서 전부 검색하여, 각 지분별 총 합을 보여줄 수 있도록 셋팅해야함.
-
-    실행 흐름
-    1. 유저가 인생네컷에 로그인하면 머신을 가진 사용자를 한해서 machineGroup에서 해당 유저를 찾음
-    2. 해당 유저가 가지고 있는 모든 서버를 검색
-    3. machineGroup에 있는 values필드를 이용해서, 해당 유저가 각 머신에 대해서 얼마의 지분을 가지고 있는지 데이터를 가져옴.
-    4. 가져온 각 머신에 대한 지분 값을 ejs로 각각 쏴버림
-    5. ajax와 nodejs에서 온 값을 이용해서 각각 나누어 버림.
-    6. 사용자화면에서 "내가 체굴한 양"부분에 나눈 값들의 합을 표시해버림.
-*/
