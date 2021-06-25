280c91dc29b8db34148acf8b8ee85387f837357a
조성윤 <lordloop4700@gmail.com>
2021-06-25 20:26:42 +0900

    react-native-navigation stack구조랑 tabbar구조 연습함

--- reactnative/note.js
+++ reactnative/note.js
@@ -2,3 +2,11 @@
 <StyledTextInput value={name} onChangeText={text => setName(text)} placeholder="name" ref={refName} returnKeyType="next" onSubmitEditing={() => refEmail.current.focus()} />
 
 //context는 어플리케이션 전체에서 알고 있어야 하는 로그인 상태를 관리하거나, 애플리케이션의 테마 혹은 설정 상태를 관리할 때 유용하게 쓰일수 있다
+
+//네비게이션
+npm install --save @react-navigation/native
+expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
+
+//page192 로딩화면과 아이콘
+//page282 헤더 감추기는 생략
+//tabBarOptions에 대해서 잘 찾지 못함(책에 있는 내용이 안됨.)
