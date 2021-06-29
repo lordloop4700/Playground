bae5e90db67a132cd3491fd39ab30328ced863c8
조성윤 <lordloop4700@gmail.com>
2021-06-29 15:27:08 +0900

    react-native-simple-chat

--- reactnative/note.js
+++ reactnative/note.js
@@ -6,7 +6,10 @@
 //네비게이션
 npm install --save @react-navigation/native
 expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
-
+npm install @react-navigation/stack @react-navigation/bottom-tabs
 //page192 로딩화면과 아이콘
 //page282 헤더 감추기는 생략
 //tabBarOptions에 대해서 잘 찾지 못함(책에 있는 내용이 안됨.)
+
+//화면 다른곳을 눌렀을 때 키보드 포커스가 사라지는 코드
+page333
